a0c750b222bfb2cad7c1bc905fdc0249f37e6120
niaa26 <nia.pavliashvili@eleve.lfb.es>
2025-05-05 19:41:39 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const connectDB = require('./config/db');
+
+const authRoutes = require('./routes/authRoutes');
+const productRoutes = require('./routes/productRoutes');
+
+dotenv.config();
+connectDB();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('API is running...');
+});
+
+app.use('/api/users', authRoutes);
+app.use('/api/products', productRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
